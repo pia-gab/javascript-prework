2bfe5fadf6b14ee68f71d6a8f4a7a5ee47c31bd6
gg <piasek.gaba@gmail.com>
2019-10-29 21:53:14 +0100

    add modified js and html, task 5.3

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let computerMove = `kamień`;
+let playerMove=`papier`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
+
+{
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+
+
