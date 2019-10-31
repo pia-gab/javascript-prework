77ac2042e1e9f426c03b6b0ebd482962bd50a506
gg <piasek.gaba@gmail.com>
2019-10-31 13:52:12 +0100

    modify functions and script

--- js/script.js
+++ js/script.js
@@ -2,15 +2,8 @@ let computerMove = `kamień`;
 let playerMove=`papier`;
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
 
-{
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+
 
 
 
